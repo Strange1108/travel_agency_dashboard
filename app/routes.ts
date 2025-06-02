06bdd39f4dbf7307d493d564df665e08fb4926ae
Mahendra Jadav <jadavmahendra972@gmail.com>
2025-06-02 16:09:55 +0530

    Implement Auth UI and Logout functionality

--- app/routes.ts
+++ app/routes.ts
@@ -2,6 +2,7 @@
 import {type RouteConfig, route, layout} from "@react-router/dev/routes";
 
 export default [
+    route('sign-in', 'routes/root/sign-in.tsx'),
     layout("routes/admin/AdminLayout.tsx", [
         route('Dashboard', 'routes/admin/Dashboard.tsx'),
         route('AllUsers', 'routes/admin/AllUsers.tsx')
--- app/routes/admin/Dashboard.tsx
+++ app/routes/admin/Dashboard.tsx
@@ -1,11 +1,19 @@
 import { StatsCard, TripCard } from "components";
 import Header from "components/Header";
+import { getUser } from "~/appwrite/auth";
 import { dashboardStats, user, allTrips} from "~/constants";
+import type { Route } from "./+types/Dashboard";
 
 const { totalUsers, usersJoined, totalTrips, tripsCreated, userRole } =
     dashboardStats;
 
-const Dashboard = () => {
+    export async function clientLoader(){
+      const user = await getUser();
+      return user;
+    }
+
+const Dashboard = ({loaderData}: Route.ComponentProps) => {
+  const user = loaderData as User | null;
   return (
     <main className="dashboard wrapper">
       <Header
