94427c712e75253cdd84b4f223166a94d60a7473
Mahendra Jadav <jadavmahendra972@gmail.com>
2025-06-02 18:04:37 +0530

    integrate Sentry for error tracking

---
+++ instrument.server.mjs
@@ -0,0 +1,14 @@
+import * as Sentry from "@sentry/react-router";
+import { nodeProfilingIntegration } from '@sentry/profiling-node';
+
+Sentry.init({
+  dsn: "https://8b06d466af78291a9337366bf50d7ae6@o4507199841501184.ingest.de.sentry.io/4507199849431120",
+  
+  // Adds request headers and IP for users, for more info visit:
+  // https://docs.sentry.io/platforms/javascript/guides/react-router/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+  
+  integrations: [nodeProfilingIntegration()],
+  tracesSampleRate: 1.0, // Capture 100% of the transactions
+  profilesSampleRate: 1.0, // profile every transaction
+});
