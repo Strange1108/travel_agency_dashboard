6c87d88b6653450c4e33ca685d79bd6d49054705
Mahendra Jadav <jadavmahendra972@gmail.com>
2025-06-04 12:19:11 +0530

    Implement Addnew trip form

--- app/routes.ts
+++ app/routes.ts
@@ -5,6 +5,9 @@ export default [
     route('sign-in', 'routes/root/sign-in.tsx'),
     layout("routes/admin/AdminLayout.tsx", [
         route('Dashboard', 'routes/admin/Dashboard.tsx'),
-        route('AllUsers', 'routes/admin/AllUsers.tsx')
+        route('AllUsers', 'routes/admin/AllUsers.tsx'),
+        route('trips', 'routes/admin/Trips.tsx'),
+        route('trips/create', 'routes/admin/CreateTrip.tsx')
+
     ]),
 ] satisfies RouteConfig;
